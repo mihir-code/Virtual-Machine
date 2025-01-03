a6611a9344bdd84738ae20f9cafcd889d013b362
Mihir <mihir.motukuri@gmail.com>
2025-01-02 17:16:57 -0800

    worked on the instruction set part of the code

--- test.c
+++ test.c
@@ -45,9 +45,86 @@ enum{
 
 
 int main(int argc, const char* argv[]){
-    printf("hello");
-    return 0;
+    if (argc < 2){
+        /*show usage string */
+        printf("lc3 [image-file1]... \n");
+        exit(2);
+    }
+    for (int j = 1; j < argc; ++j){
+        if(!read_image(argv[j])){
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+    @{Load Arguments}
+    @{Setup}
+    /* setting the Z flag since it's positive */
+    reg[R_COND] = FL_ZRO;
 
+    /* set the PC register to starting position - PC holds the memory address of the next executable instruction */
+    /* 0x300 is default*/
+
+    enum { PC_START =  0x3000};
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while(running)
+    {
+        /* Fetch the data*/
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+        
+        switch(op){
+            case OP_ADD:
+                @{ADD}
+                break;
+            case OP_AND:
+                @{AND}
+                break;
+            case OP_NOT:
+                @{NOT}
+                break;
+            case OP_BR:
+                @{BR}
+                break;
+            case OP_JMP:
+                @{JMP}
+                break;
+            case OP_JSR:
+                @{JSR}
+                break;
+            case OP_LD:
+                @{LD}
+                break;
+            case OP_LDI:
+                @{LDI}
+                break;
+            case OP_LDR:
+                @{LDR}
+                break;
+            case OP_LEA:
+                @{LEA}
+                break;
+            case OP_ST:
+                @{ST}
+                break;
+            case OP_STI:
+                @{STI}
+                break;
+            case OP_STR:
+                @{STR}
+                break;
+            case OP_TRAP:
+                @{TRAP}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                @{BAD OPCODE}
+                break;
+        }
+    }
+    @{Shutdown}
 }
 
 #define MEMORY_MAX(1 << 16)
