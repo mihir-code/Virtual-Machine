bc9c116fc8c0a235027216edb7cb85ad5610cd99
Mihir <mihir.motukuri@gmail.com>
2025-01-02 17:37:27 -0800

    set up the method to update flags

--- test.c
+++ test.c
@@ -130,3 +130,17 @@ int main(int argc, const char* argv[]){
 #define MEMORY_MAX(1 << 16)
 uint16_t memory[MEMORY_MAX]; /* There are around 65536, think binary*/
 uint16_t reg[R_COUNT];
+
+void update_flags(uint16_t r){
+    if(reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else{
+        reg[R_COND] = FL_POS;
+    }
+}
