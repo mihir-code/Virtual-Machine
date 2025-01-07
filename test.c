8568957f6e03753c955eb48cdde108583cae22e2
Mihir <mihir.motukuri@gmail.com>
2025-01-07 14:31:33 -0800

    added the and operator

--- test.c
+++ test.c
@@ -181,3 +181,21 @@ void update_flags(uint16_t r){
 
     update_flags(r0);
 }
+
+/* this is the and operator, not the add operator*/
+{
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+
+    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+    if(imm_flag){
+        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+        reg[r0] = reg[r1] & imm5;
+    }
+    else{
+        uint16_t r2 = instr & 0x7;
+        reg[r0] = reg[r1] & reg[r2];
+    }
+    update_flags(r0);
+}
