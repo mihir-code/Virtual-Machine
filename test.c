7dbb43d5a83e008945098739356f249ec080e2ed
Mihir <mihir.motukuri@gmail.com>
2025-01-09 16:27:15 -0800

    added load register, load effective addressm and store

--- test.c
+++ test.c
@@ -186,13 +186,28 @@ int main(int argc, const char* argv[]){
                 }
                 break;
             case OP_LDR:
-                @{LDR}
+                { /*this uses a base register*/
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    uint16_t offset = sign_extend(inst & 0x3F, 6);
+                    reg[r0] = mem_read(reg[r1] + offset);
+                    update_flags(r0);
+                }
                 break;
             case OP_LEA:
-                @{LEA}
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+                    reg[r0] = reg[R_PC] + pc_offset;
+                    update_flags(r0);
+                }
                 break;
             case OP_ST:
-                @{ST}
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+                    mem_write(reg[R_PC] + pc_offset, reg[r0]);
+                }
                 break;
             case OP_STI:
                 @{STI}
